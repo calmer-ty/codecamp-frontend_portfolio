14f29b37ef0f1f44775fa038f41013d7d669d7ab
Calmer_com <ty.calmer96@gmail.com>
2024-01-09 14:11:25 +0900

    button 가리기 / page active 추가

--- src/components/commons/pagination/Pagination.presenter.tsx
+++ src/components/commons/pagination/Pagination.presenter.tsx
@@ -4,20 +4,27 @@ import type { IPaginationUIProps } from "./Pagination.types";
 export default function PaginationUI(props: IPaginationUIProps): JSX.Element {
   return (
     <S.Wrapper>
-      <button onClick={props.onClickPrevPage}>{"<"}</button>
-      {new Array(10).fill(1).map((_, index) => (
-        <S.Page
-          key={props.startPage + index}
-          id={String(props.startPage + index)}
-          onClick={props.onClickPage}
-          style={{ fontSize: "20px", margin: "5px" }}
-          isActive={props.startPage + index === props.activedPage}
-        >
-          {props.startPage + index}
-        </S.Page>
-      ))}
+      {props.startPage !== 1 && (
+        <button onClick={props.onClickPrevPage}>{"<"}</button>
+      )}
+      {new Array(10).fill(1).map(
+        (_, index) =>
+          props.startPage + index <= props.lastPage && (
+            <S.Page
+              key={props.startPage + index}
+              id={String(props.startPage + index)}
+              onClick={props.onClickPage}
+              style={{ fontSize: "20px", margin: "5px" }}
+              isActive={props.startPage + index === props.activedPage}
+            >
+              {props.startPage + index}
+            </S.Page>
+          )
+      )}
 
-      <button onClick={props.onClickNextPage}>{">"}</button>
+      {props.lastPage - props.startPage > 10 && (
+        <button onClick={props.onClickNextPage}>{">"}</button>
+      )}
     </S.Wrapper>
   );
 }
