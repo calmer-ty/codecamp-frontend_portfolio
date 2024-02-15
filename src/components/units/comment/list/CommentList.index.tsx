adde6333c3d3b209a9305b8c15e971c294e0a902
Calmer_com <ty.calmer96@gmail.com>
2024-02-15 16:16:13 +0900

    무한스크롤 훅 추가

--- src/components/units/comment/list/CommentList.index.tsx
+++ src/components/units/comment/list/CommentList.index.tsx
@@ -1,31 +1,11 @@
-import CommentItem from "../../../commons/comments/board/CommentItem.index";
-import InfiniteScroll from "react-infinite-scroller";
-import { useFetchBoardComments } from "../../../commons/hooks/queries/useFetchBoardComment";
-
 import * as S from "./CommentList.styles";
+import CommentItem from "../../../commons/comments/board/CommentItem.index";
+
+import InfiniteScroll from "react-infinite-scroller";
+import { useInfiniteScroll } from "../../../commons/hooks/customs/useInfiniteScroll";
+
 export default function CommentListUI(): JSX.Element {
-  const { data, fetchMore } = useFetchBoardComments();
-  // 댓글 무한 스크롤
-  const onLoadMore = (): void => {
-    if (data === undefined) return;
-    void fetchMore({
-      variables: {
-        page: Math.ceil(data?.fetchBoardComments.length / 10) + 1,
-      },
-      updateQuery: (prev, { fetchMoreResult }) => {
-        if (fetchMoreResult?.fetchBoardComments === undefined)
-          return { fetchBoardComments: [...prev.fetchBoardComments] };
-
-        return {
-          fetchBoardComments: [
-            ...prev.fetchBoardComments,
-            ...fetchMoreResult.fetchBoardComments,
-          ],
-        };
-      },
-    });
-  };
-
+  const { data, onLoadMore } = useInfiniteScroll();
   return (
     <>
       <S.Wrapper>
