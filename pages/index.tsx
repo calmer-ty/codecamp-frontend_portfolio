c19e74c65c345222595c8836486c8626181c2992
Calmer <ty.calmer96@gmail.com>
2024-06-01 13:10:47 +0900

    feat: 슬라이드 자동플레이 스피드 값 변경

--- pages/index.tsx
+++ pages/index.tsx
@@ -38,7 +38,7 @@ export default function Home(): JSX.Element {
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 4000,
+    autoplaySpeed: 5000,
   };
 
   return (
