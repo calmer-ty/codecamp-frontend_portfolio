f828a681355d90a63efd92e21d835708bca06366
Calmer <ty.calmer96@gmail.com>
2024-06-09 23:15:01 +0900

    fix: 필요없는 코드 제거

--- pages/products/[useditemId]/index.tsx
+++ pages/products/[useditemId]/index.tsx
@@ -1,8 +1,5 @@
 import ProductDetail from "../../../src/components/units/product/detail/ProductDetail.index";
-import { useFetchProducts } from "../../../src/components/commons/hooks/queries/product/useFetchProducts";
 
 export default function ProductsDetailPage(): JSX.Element {
-  const aaa = useFetchProducts();
-  console.log(aaa);
   return <ProductDetail />;
 }
