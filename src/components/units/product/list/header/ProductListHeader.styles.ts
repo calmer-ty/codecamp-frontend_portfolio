713bf08953a7cdaf4225304708dc44ba3739f235
Calmer <ty.calmer96@gmail.com>
2024-06-09 20:26:58 +0900

    fix: 베스트상품 header 태그로 변경

--- src/components/units/product/list/header/ProductListHeader.styles.ts
+++ src/components/units/product/list/header/ProductListHeader.styles.ts
@@ -4,7 +4,7 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-export const Header = styled.section`
+export const Header = styled.header`
   display: flex;
   flex-direction: column;
   width: 100%;
