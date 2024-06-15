a79314ccc730d76db6122b79f2f9256d38934b2f
Calmer <ty.calmer96@gmail.com>
2024-06-15 16:11:30 +0900

    fix: key 중복 문제 수정

--- src/components/commons/element/tags/view/01/index.tsx
+++ src/components/commons/element/tags/view/01/index.tsx
@@ -2,5 +2,5 @@ import * as S from "./styles";
 import type { ITagsView } from "./types";
 
 export default function TagsView01(props: ITagsView): JSX.Element {
-  return <S.Tags>{props.tags?.map((el) => <S.TagItem key={el}># {el}</S.TagItem>)}</S.Tags>;
+  return <S.Tags>{props.tags?.map((el, index) => <S.TagItem key={`${el}_${index}`}># {el}</S.TagItem>)}</S.Tags>;
 }
