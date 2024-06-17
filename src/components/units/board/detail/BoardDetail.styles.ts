1a09446fbc7f30a62e6efb62a434c913f51c7850
Calmer_com <ty.calmer96@gmail.com>
2024-06-17 16:04:48 +0900

    fix: 게시판 디테일 스타일 수정

--- src/components/units/board/detail/BoardDetail.styles.ts
+++ src/components/units/board/detail/BoardDetail.styles.ts
@@ -1,16 +1,10 @@
 import styled from "@emotion/styled";
-import { mediaQueries } from "../../../../commons/styles/globalStyles";
-import { css } from "@emotion/react";
 
 export const CardWrap = styled.section`
   display: flex;
   flex-direction: column;
   width: 100%;
-  padding: 100px;
+  padding: 60px;
   border: 1px solid #bdbdbd;
   background-color: #fff;
-
-  ${mediaQueries.desktop(css`
-    padding: 100px 50px;
-  `)}
 `;
